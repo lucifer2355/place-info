5b6494726b56e2dd57c7d36b1537a05beb00b9ea
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-02-21 16:13:45 +0530

    add file system

--- store/actions/places.jsx
+++ store/actions/places.jsx
@@ -1,5 +1,20 @@
+import * as FileSystem from "expo-file-system";
 export const ADD_PLACE = "ADD_PLACE";
 
 export const addPlace = (title, image) => {
-  return { type: ADD_PLACE, placeData: { title: title, image: image } };
+  return async dispatch => {
+    const fileName = image.split("/").pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath
+      });
+    } catch (err) {
+      throw err;
+    }
+
+    dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
+  };
 };
