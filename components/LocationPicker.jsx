e948866bdb64f79a5d958d00f90e4553a564c3a2
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-02-24 13:19:58 +0530

    show map preview

--- components/LocationPicker.jsx
+++ components/LocationPicker.jsx
@@ -10,6 +10,7 @@ import {
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
 import Colors from "../constants/Colors";
+import MapPreview from "./MapPreview";
 
 const LocationPicker = props => {
   const [isFetching, setIsFetching] = useState(false);
@@ -55,13 +56,13 @@ const LocationPicker = props => {
 
   return (
     <View style={styles.locationPicker}>
-      <View style={styles.mapPreview}>
+      <MapPreview style={styles.mapPreview} location={pickedLocation}>
         {isFetching ? (
           <ActivitiIndicator size='large' color={Colors.primary} />
         ) : (
           <Text>No location chosen yet.</Text>
         )}
-      </View>
+      </MapPreview>
       <Button
         title='Get User Location'
         color={Colors.primary}
@@ -81,9 +82,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 150,
     borderColor: "#ccc",
-    borderWidth: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    borderWidth: 1
   }
 });
 
