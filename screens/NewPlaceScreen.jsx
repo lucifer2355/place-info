ac00a9989257a629d2c34002888f15f2a94f6770
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-02-25 16:54:50 +0530

    fix some bug

--- screens/NewPlaceScreen.jsx
+++ screens/NewPlaceScreen.jsx
@@ -1,83 +1,89 @@
-import React, { useState, useCallback } from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
-import { TextInput, ScrollView } from "react-native-gesture-handler";
-import Colors from "../constants/Colors";
-import { useDispatch } from "react-redux";
-import * as placesActions from "../store/actions/places";
-import ImagePicker from "../components/ImagePicker";
-import LocationPicker from "../components/LocationPicker";
+import React, { useState, useCallback } from 'react';
+import {
+	ScrollView,
+	View,
+	Button,
+	Text,
+	TextInput,
+	StyleSheet
+} from 'react-native';
+import { useDispatch } from 'react-redux';
+
+import Colors from '../constants/Colors';
+import * as placesActions from '../store/actions/places';
+import ImagePicker from '../components/ImagePicker';
+import LocationPicker from '../components/LocationPicker';
 
 const NewPlaceScreen = props => {
-  const [titleValue, setTitleValue] = useState("");
-  const [selectedImage, setSelectedImage] = useState();
-  const [selectedLocation, setSelectedLocation] = useState();
+	const [titleValue, setTitleValue] = useState('');
+	const [selectedImage, setSelectedImage] = useState();
+	const [selectedLocation, setSelectedLocation] = useState();
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const titleChangeHandler = text => {
-    setTitleValue(text);
-  };
+	const titleChangeHandler = text => {
+		// you could add validation
+		setTitleValue(text);
+	};
 
-  const imageTakenHandler = imagePath => {
-    setSelectedImage(imagePath);
-  };
+	const imageTakenHandler = imagePath => {
+		setSelectedImage(imagePath);
+	};
 
-  const locationPickedHandler = useCallback(location => {
-    setSelectedLocation(location);
-  }, []);
+	const locationPickedHandler = useCallback(location => {
+		setSelectedLocation(location);
+	}, []);
 
-  const savePlaceHandler = () => {
-    dispatch(
-      placesActions.addPlace(titleValue, selectedImage, selectedLocation)
-    );
-    props.navigation.goBack();
-  };
+	const savePlaceHandler = () => {
+		dispatch(
+			placesActions.addPlace(titleValue, selectedImage, selectedLocation)
+		);
+		props.navigation.goBack();
+	};
 
-  return (
-    <ScrollView>
-      <View style={styles.form}>
-        <Text style={styles.label}>Title</Text>
-        <TextInput
-          style={styles.textInput}
-          onChangeText={titleChangeHandler}
-          value={titleValue}
-        />
-        <ImagePicker onImageTaken={imageTakenHandler} />
-        <LocationPicker
-          navigation={props.navigation}
-          onLocationPicked={locationPickedHandler}
-        />
-        <Button
-          title='Save Place'
-          color={Colors.primary}
-          onPress={savePlaceHandler}
-        />
-      </View>
-    </ScrollView>
-  );
+	return (
+		<ScrollView>
+			<View style={styles.form}>
+				<Text style={styles.label}>Title</Text>
+				<TextInput
+					style={styles.textInput}
+					onChangeText={titleChangeHandler}
+					value={titleValue}
+				/>
+				<ImagePicker onImageTaken={imageTakenHandler} />
+				<LocationPicker
+					navigation={props.navigation}
+					onLocationPicked={locationPickedHandler}
+				/>
+				<Button
+					title="Save Place"
+					color={Colors.primary}
+					onPress={savePlaceHandler}
+				/>
+			</View>
+		</ScrollView>
+	);
 };
 
 NewPlaceScreen.navigationOptions = {
-  headerTitle: "Add Place"
+	headerTitle: 'Add Place'
 };
 
 const styles = StyleSheet.create({
-  form: {
-    margin: 30
-  },
-
-  label: {
-    fontSize: 18,
-    marginBottom: 15
-  },
-
-  textInput: {
-    borderBottomColor: "#ccc",
-    borderBottomWidth: 1,
-    marginBottom: 15,
-    paddingVertical: 4,
-    paddingHorizontal: 2
-  }
+	form: {
+		margin: 30
+	},
+	label: {
+		fontSize: 18,
+		marginBottom: 15
+	},
+	textInput: {
+		borderBottomColor: '#ccc',
+		borderBottomWidth: 1,
+		marginBottom: 15,
+		paddingVertical: 4,
+		paddingHorizontal: 2
+	}
 });
 
 export default NewPlaceScreen;
