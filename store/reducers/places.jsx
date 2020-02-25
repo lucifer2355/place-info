2204f84f7272253e653bfeeea7b1e3c558640de8
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-02-25 17:13:57 +0530

    fix some bug

--- store/reducers/places.jsx
+++ store/reducers/places.jsx
@@ -1,40 +1,39 @@
-import { ADD_PLACE, SET_PLACES } from "../actions/places";
-import Place from "../../models/place";
+import { ADD_PLACE, SET_PLACES } from '../actions/places';
+import Place from '../../models/place';
 
 const initialState = {
-  places: []
+	places: []
 };
 
 export default (state = initialState, action) => {
-  switch (action.type) {
-    case SET_PLACES:
-      return {
-        places: action.places.map(
-          place =>
-            new Place(
-              place.id.toString(),
-              place.title,
-              place.imageUri,
-              place.address,
-              place.lat,
-              place.lng
-            )
-        )
-      };
-
-    case ADD_PLACE:
-      const newPlace = new Place(
-        action.placeData.id.toString(),
-        action.placeData.title,
-        action.placeData.image,
-        action.placeData.address,
-        action.placeData.coords.lat,
-        action.placeData.coords.lng
-      );
-      return {
-        places: state.places.concat(newPlace)
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case SET_PLACES:
+			return {
+				places: action.places.map(
+					pl =>
+						new Place(
+							pl.id.toString(),
+							pl.title,
+							pl.imageUri,
+							pl.address,
+							pl.lat,
+							pl.lng
+						)
+				)
+			};
+		case ADD_PLACE:
+			const newPlace = new Place(
+				action.placeData.id.toString(),
+				action.placeData.title,
+				action.placeData.image,
+				action.placeData.address,
+				action.placeData.coords.lat,
+				action.placeData.coords.lng
+			);
+			return {
+				places: state.places.concat(newPlace)
+			};
+		default:
+			return state;
+	}
 };
