db11c0bc22cb64f8061793bc05115dc48f7c1d71
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-02-25 11:37:02 +0530

    add save button in map screen

--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -1,6 +1,13 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect, useCallback } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Platform
+} from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import Colors from "../constants/Colors";
 
 const MapScreen = props => {
   const [selectedLocation, setSelectedLocation] = useState();
@@ -18,6 +25,14 @@ const MapScreen = props => {
       lng: event.nativeEvent.coordinate.longitude
     });
   };
+
+  const savePickedLocationHandler = useCallback(() => {
+    props.navigation.goBack();
+  }, []);
+
+  useEffect(() => {
+    props.navigation.setParams({ saveLocation: savePickedLocationHandler });
+  }, [savePickedLocationHandler]);
 
   let markerCoordinates;
 
@@ -41,9 +56,29 @@ const MapScreen = props => {
   );
 };
 
+MapScreen.navigationOptions = navData => {
+  const saveFn = navData.navigation.getParam("saveLocation");
+  return {
+    headerRight: (
+      <TouchableOpacity style={styles.headerButton} onPress={saveFn}>
+        <Text style={styles.headerButtonText}>Save</Text>
+      </TouchableOpacity>
+    )
+  };
+};
+
 const styles = StyleSheet.create({
   map: {
     flex: 1
+  },
+
+  headerButton: {
+    marginHorizontal: 20
+  },
+
+  headerButtonText: {
+    fontSize: 16,
+    color: Platform.OS === "android" ? "white" : Colors.primary
   }
 });
 
